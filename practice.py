125fc6f65be186c1f8da6bd3e899df05097c121f
saurabhchobey <52131942+saurabhchobey@users.noreply.github.com>
2020-04-03 22:19:28 +0530

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,20 @@
+input=[]
+k=int(input())
+output=int(input())
+for i in range(k):
+   j=int(input())
+   input.append(j)
+ 
+l=0
+if k%2==0:
+  l=1
+else:
+  l=0
+if l==0:
+  print(input(int(k/2))
+else:
+  print(input(int((k-1)/2))
+  
+ 
+ 
+
